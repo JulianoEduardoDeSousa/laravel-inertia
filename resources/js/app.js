9772483433cb66c3d23286fde34da5a7c823e78d
Juliano Eduardo de Sousa <thejuliano22@gmail.com>
2024-02-20 11:58:47 -0300

    antes do await

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,15 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: async (name) => {
+        const page = resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'))
+        
+
+        return page;
+    },
+
+
+
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
